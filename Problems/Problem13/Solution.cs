d9a46e22dd96ceafa2ed6fe54812c4ba0162b847
Dmitry Kyptsov <khoth@ya.ru>
2023-02-04 02:36:51 +0300

    Problem 13 is solved
    13. https://leetcode.com/problems/roman-to-integer/

---
+++ Problems/Problem13/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace Problem13
+{
+    public class Solution
+    {
+        private readonly IReadOnlyDictionary<char, int> _symbolWeights = new Dictionary<char, int>
+        {
+            {'I', 1 },
+            {'V', 5 },
+            {'X', 10 },
+            {'L', 50 },
+            {'C', 100 },
+            {'D', 500 },
+            {'M', 1000 }
+        };
+
+        public int RomanToInt(string input)
+        {
+            var result = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i < input.Length - 1 &&
+                    (input[i] == 'I' && (input[i + 1] == 'V' || input[i + 1] == 'X') ||
+                     input[i] == 'X' && (input[i + 1] == 'L' || input[i + 1] == 'C') ||
+                     input[i] == 'C' && (input[i + 1] == 'D' || input[i + 1] == 'M')))
+                {
+                    result -= _symbolWeights[input[i]];
+                }
+                else
+                {
+                    result += _symbolWeights[input[i]];
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ UnitTests/Problem13.cs
@@ -0,0 +1,19 @@
+﻿using Problem13;
+
+namespace UnitTests
+{
+    public class Problem13
+    {
+        [Theory]
+        [InlineData("III", 3)]
+        [InlineData("LVIII", 58)]
+        [InlineData("MCMXCIV", 1994)]
+        [InlineData("IX", 9)]
+        public void RomanToInt_Returns_ExpectedResult(string input, int expectedResult)
+        {
+            var solution = new Solution();
+            var result = solution.RomanToInt(input);
+            result.Should().Be(expectedResult);
+        }
+    }
+}
