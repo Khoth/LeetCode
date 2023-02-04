3890a7f989c60826dbf879bc9defaba841a9fad7
Dmitry Kyptsov <khoth@ya.ru>
2023-02-04 12:26:44 +0300

    Problem 14 is solved
    14. https://leetcode.com/problems/longest-common-prefix/

---
+++ Problems/Problem0014/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace Problems.Problem0014
+{
+    public class Solution
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            var maxLength = strs.Min(x => x.Length);
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                if (strs.Any(x => x[i] != strs.First()[i]))
+                {
+                    return strs.First()[..i];
+                }
+            }
+
+            return strs.First()[..maxLength];
+        }
+    }
+}
---
+++ UnitTests/Problem0014.cs
@@ -0,0 +1,18 @@
+﻿using Problems.Problem0014;
+
+namespace UnitTests
+{
+    public class Problem0014
+    {
+        [Theory]
+        [InlineData(new[] { "flower", "flow", "flight" }, "fl")]
+        [InlineData(new[] { "dog", "racecar", "car" }, "")]
+        [InlineData(new[] { "a" }, "a")]
+        public void LongestCommonPrefix_Returns_ExpectedResult(string[] strs, string expectedResult)
+        {
+            var solution = new Solution();
+            var result = solution.LongestCommonPrefix(strs);
+            result.Should().Be(expectedResult);
+        }
+    }
+}
