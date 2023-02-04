f704fcf7276f5a322d6d911f9fda57b21acdf5c7
Dmitry Kyptsov <khoth@ya.ru>
2023-02-04 19:24:58 +0300

    Problems 175 and 182 are solved
    175. https://leetcode.com/problems/combine-two-tables/
    182. https://leetcode.com/problems/duplicate-emails/

---
+++ Problems/Problem0175/Solution.sql
@@ -0,0 +1,3 @@
+﻿SELECT p.firstName, p.lastName, a.city, a.state
+FROM Person p (NOLOCK)
+LEFT JOIN Address a (NOLOCK) ON p.personId = a.personId
---
+++ Problems/Problem0182/Solution.sql
@@ -0,0 +1,4 @@
+﻿SELECT email
+FROM Person
+GROUP BY email
+HAVING COUNT(email) > 1
