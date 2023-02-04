d90c176f7091e342b026c04a2159bda3eafeca48
Dmitry Kyptsov <khoth@ya.ru>
2023-02-04 19:10:35 +0300

    Problem 7 is solved
    7. https://leetcode.com/problems/reverse-integer/

---
+++ Problems/Problem0007/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace Problems.Problem0007
+{
+    public class Solution
+    {
+        public int Reverse(int x)
+        {
+            try
+            {
+                int.TryParse(string.Join("", Math.Abs(x).ToString().Reverse()), out int result);
+                return x >= 0 ? result : -result;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ UnitTests/Problem0007.cs
@@ -0,0 +1,18 @@
+﻿using Problems.Problem0007;
+
+namespace UnitTests
+{
+    public class Problem0007
+    {
+        [Theory]
+        [InlineData(123, 321)]
+        [InlineData(-123, -321)]
+        [InlineData(120, 21)]
+        public void Reverse_Returns_ExpectedResult(int input, int expectedResult)
+        {
+            var solution = new Solution();
+            var result = solution.Reverse(input);
+            result.Should().Be(expectedResult);
+        }
+    }
+}
