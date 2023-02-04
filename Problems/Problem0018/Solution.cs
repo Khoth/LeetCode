c369f9c2392d52f0ffeee5d30ddbf35286eeb4ff
Dmitry Kyptsov <khoth@ya.ru>
2023-02-05 01:19:04 +0300

    Problem 18 is solved
    18. https://leetcode.com/problems/4sum/

---
+++ Problems/Problem0018/Solution.cs
@@ -0,0 +1,87 @@
+﻿namespace Problems.Problem0018
+{
+    public class Solution
+    {
+        private const int _resultElementsCount = 4;
+
+        public IList<IList<int>> FourSum(int[] nums, int target)
+        {
+            if (nums.Length < _resultElementsCount)
+            {
+                return new List<IList<int>>(0);
+            }
+
+            var result = new List<IList<int>>();
+
+            if (HasSameElements(nums))
+            {
+                var items = ResultFromSameElements(nums, target);
+
+                if (items.Count > 0)
+                {
+                    result.Add(items);
+                    return result;
+                }
+            }
+
+            for (int a = 0; a < nums.Length; a++)
+            {
+                for (int b = a + 1; b < nums.Length; b++)
+                {
+                    for (int c = b + 1; c < nums.Length; c++)
+                    {
+                        for (int d = c + 1; d < nums.Length; d++)
+                        {
+                            if (nums[a] + nums[b] + nums[c] + nums[d] == target)
+                            {
+                                var items = new[] { nums[a], nums[b], nums[c], nums[d] };
+
+                                if (HasSameElements(items))
+                                {
+                                    items = ResultFromSameElements(items, target).ToArray();
+                                }
+                                if (items.Length == 0)
+                                {
+                                    continue;
+                                }
+
+                                Array.Sort(items);
+
+                                if (result.Any(x => x.SequenceEqual(items)))
+                                {
+                                    continue;
+                                }
+
+                                result.Add(items);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static bool IsInRange(int value) =>
+            value > int.MinValue / _resultElementsCount &&
+            value < int.MaxValue / _resultElementsCount;
+
+        private static bool HasSameElements(int[] nums) =>
+            nums.Distinct().Count() == 1;
+
+        private static IList<int> ResultFromSameElements(int[] nums, int target)
+        {
+            if (nums[0] * _resultElementsCount == target)
+            {
+                if (!IsInRange(nums[0]))
+                {
+                    return Array.Empty<int>();
+                }
+
+                return Enumerable.Repeat(nums[0], _resultElementsCount).ToList();
+            }
+
+            return Array.Empty<int>();
+        }
+    }
+}
---
+++ UnitTests/Problem0018.cs
@@ -0,0 +1,28 @@
+﻿using Problems.Problem0018;
+
+namespace UnitTests
+{
+    public class Problem0018
+    {
+        public static IEnumerable<object[]> TestData = new List<object[]>
+        {
+            new object[] { new int[]{ 1, 0, -1, 0, -2, 2 }, 0, new int[][] { new int[] { -2, -1, 1, 2 }, new int[] { -2, 0, 0, 2 }, new int[] { -1, 0, 0, 1 } } },
+            new object[] { Enumerable.Repeat(2, 5).ToArray(), 8, new int[][] { new int[] { 2, 2, 2, 2 } } },
+            new object[] { new int[]{ -5, 5, 4, -3, 0, 0, 4, -2 }, 4, new int[][] { new int[] { -5, 0, 4, 5 }, new int[] { -3, -2, 4, 5 } } },
+            new object[] { Enumerable.Repeat(2, 200).ToArray(), 8, new int[][] { new int[] { 2, 2, 2, 2 } } },
+            new object[] { new int[]{ 0 }, 0, Array.Empty<int[]>() },
+            new object[] { new int[] { 1000000000, 1000000000, 1000000000, 1000000000 }, -294967296, Array.Empty<int[]>() },
+            new object[] { new int[] { -1000000000, -1000000000, 1000000000, -1000000000, -1000000000 }, 294967296, Array.Empty<int[]>() },
+            new object[] { new int[]{ 0, 0, 0, 0 }, 1, Array.Empty<int[]>() },
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void FourSum_Returns_ExpectedResult(int[] nums, int target, IList<IList<int>> expectedResult)
+        {
+            var solution = new Solution();
+            var result = solution.FourSum(nums, target);
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+    }
+}
