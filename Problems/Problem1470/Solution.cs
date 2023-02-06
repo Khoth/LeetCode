33e8c08fc297ff5b4191519c6ef048ef5997fbe5
Dmitry Kyptsov <khoth@ya.ru>
2023-02-06 23:05:19 +0300

    Problem 1470 is solved
    1470. https://leetcode.com/problems/shuffle-the-array/

---
+++ Problems/Problem1470/Solution.cs
@@ -0,0 +1,13 @@
+﻿namespace Problems.Problem1470
+{
+    public class Solution
+    {
+        public int[] Shuffle(int[] nums, int n)
+        {
+            var result = new int[n * 2];
+            for (int i = 0; i < n * 2; i++)
+                result[i] = nums[i / 2 + (i % 2 == 0 ? 0 : n)];
+            return result;
+        }
+    }
+}
---
+++ UnitTests/Problem1470.cs
@@ -0,0 +1,18 @@
+﻿using Problems.Problem1470;
+
+namespace UnitTests
+{
+    public class Problem1470
+    {
+        [Theory]
+        [InlineData(new[] { 2, 5, 1, 3, 4, 7 }, 3, new[] { 2, 3, 5, 4, 1, 7 })]
+        [InlineData(new[] { 1, 2, 3, 4, 4, 3, 2, 1 }, 4, new[] { 1, 4, 2, 3, 3, 2, 4, 1 })]
+        [InlineData(new[] { 1, 1, 2, 2 }, 2, new[] { 1, 2, 1, 2 })]
+        public void Shuffle_Returns_ExpectedResult(int[] nums, int n, int[] expectedResult)
+        {
+            var solution = new Solution();
+            var result = solution.Shuffle(nums, n);
+            result.Should().BeEquivalentTo(expectedResult, options => options.WithStrictOrdering());
+        }
+    }
+}
