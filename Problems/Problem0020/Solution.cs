d14a8fbfb5b7fca4a43aefd2d537456d9dcca233
Dmitry Kyptsov <khoth@ya.ru>
2023-02-12 02:00:27 +0300

    Problem 20 is solved
    20. https://leetcode.com/problems/valid-parentheses

---
+++ Problems/Problem0020/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace Problems.Problem0020
+{
+    public class Solution
+    {
+        private readonly HashSet<(char Open, char Close)> _bracketOpenClosePairs = new()
+        {
+            ('(', ')'),
+            ('[', ']'),
+            ('{', '}'),
+        };
+
+        public bool IsValid(string s)
+        {
+            var brackets = new Stack<char>();
+
+            foreach (char c in s)
+            {
+                if (_bracketOpenClosePairs.Any(x => x.Open == c))
+                {
+                    brackets.Push(c);
+                }
+                else
+                {
+                    if (!brackets.TryPop(out char lastOpenBracket) ||
+                        lastOpenBracket != _bracketOpenClosePairs.First(x => x.Close == c).Open)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return brackets.Count == 0;
+        }
+    }
+}
---
+++ UnitTests/Problem0020.cs
@@ -0,0 +1,20 @@
+﻿using Problems.Problem0020;
+
+namespace UnitTests
+{
+    public class Problem0020
+    {
+        [Theory]
+        [InlineData("()", true)]
+        [InlineData("()[]{}", true)]
+        [InlineData("(]", false)]
+        [InlineData("[", false)]
+        [InlineData("}", false)]
+        public void IsValid_Returns_ExpectedResult(string input, bool expectedResult)
+        {
+            var solution = new Solution();
+            var result = solution.IsValid(input);
+            result.Should().Be(expectedResult);
+        }
+    }
+}
