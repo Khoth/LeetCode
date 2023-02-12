1e615113d50c5d3a28cbf78ca73f874f33851af5
Dmitry Kyptsov <khoth@ya.ru>
2023-02-12 23:11:20 +0300

    Problem 8 is solved
    8. https://leetcode.com/problems/string-to-integer-atoi

---
+++ Problems/Problem0008/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Problems.Problem0008
+{
+    public class Solution
+    {
+        public int MyAtoi(string s)
+        {
+            var match = Regex.Match(s, "^\\s*(\\+|\\-)?\\d+");
+
+            if (!match.Success)
+            {
+                return 0;
+            }
+
+            try
+            {
+                return int.Parse(match.Value);
+            }
+            catch
+            {
+                return match.Value.Contains('-')
+                    ? int.MinValue
+                    : int.MaxValue;
+            }
+        }
+    }
+}
---
+++ UnitTests/Problem0008.cs
@@ -0,0 +1,20 @@
+﻿using Problems.Problem0008;
+
+namespace UnitTests
+{
+    public class Problem0008
+    {
+        [Theory]
+        [InlineData("42", 42)]
+        [InlineData("   -42", -42)]
+        [InlineData("4193 with words", 4193)]
+        [InlineData("words and 987", 0)]
+        [InlineData("-91283472332", int.MinValue)]
+        public void MyAtoi_Returns_ExpectedResult(string input, int expectedResult)
+        {
+            var solution = new Solution();
+            var result = solution.MyAtoi(input);
+            result.Should().Be(expectedResult);
+        }
+    }
+}
