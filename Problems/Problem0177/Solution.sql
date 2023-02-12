de96cac7fd60a1a959e34dc437a8402f2014c1d5
Dmitry Kyptsov <khoth@ya.ru>
2023-02-12 10:08:58 +0300

    Problem 177 is solved
    177. https://leetcode.com/problems/second-highest-salary

---
+++ Problems/Problem0177/Solution.sql
@@ -0,0 +1,9 @@
+﻿SELECT TOP 1 sr.salary AS SecondHighestSalary
+FROM (VALUES (2)) AS r(rank)
+LEFT JOIN
+(
+    SELECT
+        DENSE_RANK() OVER(ORDER BY salary DESC) AS rank,
+        salary
+    FROM Employee
+) AS sr ON sr.rank = r.rank
