a41f9716aac20f832ef0ff39bc5f62cf17142d94
Dmitry Kyptsov <khoth@ya.ru>
2023-02-14 00:39:41 +0300

    Problem 1523 is solved
    1523. https://leetcode.com/problems/count-odd-numbers-in-an-interval-range

---
+++ Problems/Problem1523/Solution.cs
@@ -0,0 +1,8 @@
+﻿namespace Problems.Problem1523
+{
+    public class Solution
+    {
+        public int CountOdds(int low, int high) =>
+            (high - low) / 2 + (low % 2 | high % 2);
+    }
+}
---
+++ UnitTests/Problem1523.cs
@@ -0,0 +1,18 @@
+﻿using Problems.Problem1523;
+
+namespace UnitTests
+{
+    public class Problem1523
+    {
+        [Theory]
+        [InlineData(3, 7, 3)]
+        [InlineData(8, 10, 1)]
+        [InlineData(14, 17, 2)]
+        public void CountOdds_Returns_ExpectedResult(int low, int high, int expectedResult)
+        {
+            var solution = new Solution();
+            var result = solution.CountOdds(low, high);
+            result.Should().Be(expectedResult);
+        }
+    }
+}
