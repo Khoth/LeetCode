4aea16cc589b0a23aecc8aa68fc062564f5d36c0
Dmitry Kyptsov <khoth@ya.ru>
2023-02-21 09:55:58 +0300

    Problem 540 is solved
    540. https://leetcode.com/problems/single-element-in-a-sorted-array

---
+++ Problems/Problem0540/Solution.cs
@@ -0,0 +1,36 @@
+﻿namespace Problems.Problem0540
+{
+    public class Solution
+    {
+        public int SingleNonDuplicate(int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+
+            int currentCount = 1;
+            int current = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] == current)
+                {
+                    currentCount++;
+                }
+                else
+                {
+                    if (currentCount == 1)
+                    {
+                        return current;
+                    }
+
+                    currentCount = 1;
+                    current = nums[i];
+                }
+            }
+            
+            return currentCount == 1 ? current : 0;
+        }
+    }
+}
---
+++ UnitTests/Problem0540.cs
@@ -0,0 +1,19 @@
+﻿using Problems.Problem0540;
+
+namespace UnitTests
+{
+    public class Problem0540
+    {
+        [Theory]
+        [InlineData(new[] { 1, 1, 2, 3, 3, 4, 4, 8, 8 }, 2)]
+        [InlineData(new[] { 3, 3, 7, 7, 10, 11, 11 }, 10)]
+        [InlineData(new[] { 1 }, 1)]
+        [InlineData(new[] { 1, 1, 2 }, 2)]
+        public void SingleNonDuplicate_Returns_ExpectedResult(int[] nums, int expectedResult)
+        {
+            var solution = new Solution();
+            var result = solution.SingleNonDuplicate(nums);
+            result.Should().Be(expectedResult);
+        }
+    }
+}
